bf298d097df0d119e53b4b079ce95f2087d7bcc0
Prashanth Chintala <pchintala16@gmail.com>
2020-07-21 13:34:11 -0700

    Create hIndex.java

---
+++ hIndex.java
@@ -0,0 +1,29 @@
+// Time Complexity : O(log n). n is the number of elements in an array
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+class Solution {
+    public int hIndex(int[] citations) {
+        
+        if(citations==null || citations.length==0) return 0;
+        
+        int n = citations.length;
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            
+            int mid = low+(high-low)/2;
+            
+            int diff =  n - mid;
+            
+            if(citations[mid]==diff) return diff;
+            else if(diff>citations[mid]) low=mid+1;
+            else high=mid-1;
+            
+        }
+        
+        return n-low;
+    }
+}
